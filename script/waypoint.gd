9eb2b4d2259e754560551761bff329c6f6e0879e
YongHyun Yi <dldydgus1995@naver.com>
2020-02-20 21:17:36 +0900

    2020. 02. 20.
    ver 0.2
    PM 09 :17

--- script/waypoint.gd
+++ script/waypoint.gd
@@ -5,6 +5,7 @@ var clicked_start_point : Vector2
 var drag = false
 var drag_pos : Vector2
 var index : int
+var path : PoolVector2Array
 onready var unit = get_node("../..")
 
 
@@ -16,77 +17,36 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
+	# ------------------------
+	
 	if clicked == true and drag == false:
 		if clicked_start_point != get_global_mouse_position():
 			drag = true
-		# Change drag variable to true if the click start point and the current mouse position are different with the unit clicked
+	
+	# ------------------------
 	
 	if drag == true:
-		# Enable drag mode
-		# I will use two waypoints(Previous and Next Point) as if I had inserted a waypoint in the middle.
-		# Use one waypoint(Previous Point) if you move the end waypoint.
 		
 		if drag_pos != get_global_mouse_position():
-			# Run only when drag position is updated
 			
 			drag_pos = unit.nav.get_closest_point(get_global_mouse_position())
-			# Obtain a valid close position in the navigation2d polygon from the current drag position
 			
 			global_position = drag_pos
-			# update position of dragged waypoint
 			
-			#---------
-			var waypoint1
-			var waypoint2
+			# ------------------------
 			
-			var copy_left_path_array
+			var waypoint1 = get_parent().get_child(get_index()-1)
+			waypoint1.path = unit.nav.get_simple_path(waypoint1.global_position, global_position)
 			
-			var a
-			var b
+			if path:
+				var waypoint2 = get_parent().get_child(get_index()+1)
+				path = unit.nav.get_simple_path(global_position, waypoint2.global_position)
 			
-			var index_change_size
+			# ------------------------
 			
-			# The variables and methods used are almost the same as inserting a waypoint in the middle in the
+			unit.update_path_draw()
 			
-			waypoint1 = get_parent().get_child(get_index()-1)
-			if get_parent().get_child_count()-1 > get_index(): 
-				waypoint2 = get_parent().get_child(get_index()+1)
-				# If the currently selected waypoint is not the last point, Obtain the next waypoint
-			
-			var path_array = Array(unit.path)
-			if waypoint2:
-				copy_left_path_array = path_array.slice(waypoint2.index, unit.path.size()-1)
-				copy_left_path_array.remove(0)
-				# The explanation is the same as before. The only difference is whether or not we've got a waypoint2.
-			
-			a = unit.nav.get_simple_path(unit.path[waypoint1.index], global_position)
-			a.remove(0)
-			
-			if waypoint2:
-				b = unit.nav.get_simple_path(global_position, unit.path[waypoint2.index])
-				b.remove(0)
-			
-			path_array.resize(waypoint1.index+1)
-			path_array = PoolVector2Array(path_array)
-			
-			path_array.append_array(a)
-			var insert_index = path_array.size()-1
-			
-			if waypoint2:
-				path_array.append_array(b)
-				index_change_size = (path_array.size()-1) - waypoint2.index
-			
-				path_array.append_array(copy_left_path_array)
-				
-				for i in range(get_index()+1, get_parent().get_child_count()):
-					get_parent().get_child(i).index += index_change_size
-			
-			unit.path = path_array
-			index = insert_index
-			#---------
-			
-			unit.line2d.points = unit.path
-	pass
+	# ------------------------
 
 func _input(event):
 	if event.is_action_released("left_click"):
@@ -94,8 +54,6 @@ func _input(event):
 			clicked = false
 			drag = false
 			clicked_start_point = Vector2()
-		# When release with the mouse, Clicked and drag variables change to false
-		# Initialize click start point
 
 func _on_waypoint_input_event(viewport, event, shape_idx):
 	
@@ -104,10 +62,13 @@ func _on_waypoint_input_event(viewport, event, shape_idx):
 			if clicked == false:
 				clicked = true
 				clicked_start_point = event.position
-			# When the waypoint is clicked, clicked variable change to true
-			# Assign mouse event location to click start point
 
 func command_excute():
 	print("excute command!")
-	unit.move_path()
+	if path:
+		unit.path = path
+		unit.move_path()
+	else:
+		unit.path_initiate()
+		unit.move = false
 	queue_free()
